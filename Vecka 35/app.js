e087c1f34bcbe327882a7d8e29b16d8ad1baafab
Linus Romland (linusromland) <48645689+linusromland@users.noreply.github.com>
2020-08-27 12:49:46 +0200

    added Query String

--- Vecka 35/app.js
+++ Vecka 35/app.js
@@ -3,6 +3,8 @@ var http = require("http");
 http
   .createServer(function (req, res) {
     res.write("Hello Niklas!"); //write a response to the client
+    const queryObject = url.parse(req.url, true).query;
+    console.log(queryObject);
     res.end(); //end the response
   })
-  .listen(5000); //the server object listens on port 80
+  .listen(5000); //the server object listens on port 5000
