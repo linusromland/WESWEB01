4b09b6b956ea19c9c409df96445f14725eb8d518
Linus Romland <48645689+linusromland@users.noreply.github.com>
2020-09-01 10:53:32 +0200

    added borken download html thingy

--- Vecka 36/app.js
+++ Vecka 36/app.js
@@ -1,7 +1,21 @@
 const express = require('express')
+const http = require('http')
 const app = express()
 const port = 3000
+const filesDir = __dirname + "/files/"
 
-app.get('/', (req, res) => res.send('Visit https://romland.space/!'))
-app.get('/why', (req, res) => res.send('Why??'))
+app.get('/', (req, res) => res.sendFile(filesDir + 'index.html'))
+app.get('/why', (req, res) => {
+    var options = {
+        host: 'www.google.com',
+        port: 80,
+        path: '/index.html'
+      };
+      
+      http.get(options, function(res) {
+        console.log("Got response: " + res.read());
+      }).on('error', function(e) {
+        console.log("Got error: " + e.message);
+      });
+})
 app.listen(port, () => console.log(`Server running on port ${port}!`))
