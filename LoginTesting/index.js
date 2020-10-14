8541a0482607373e363a2c31d5325aa333487fd3
linusromland <48645689+linusromland@users.noreply.github.com>
2020-10-14 08:20:22 +0200

    Login still not working, now using EJS and Passport and other scary things

--- LoginTesting/index.js
+++ LoginTesting/index.js
@@ -1,30 +1,84 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const filesDir = __dirname + "/client/"
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const dbfile = require('./mongo.js');
+let express = require("express"), 
+    mongoose = require("mongoose"), 
+    passport = require("passport"), 
+    bodyParser = require("body-parser"), 
+    LocalStrategy = require("passport-local"), 
+    passportLocalMongoose =  require("passport-local-mongoose"), 
+    User = require("./models/user"); 
+  
+mongoose.set('useNewUrlParser', true); 
+mongoose.set('useFindAndModify', false); 
+mongoose.set('useCreateIndex', true); 
+mongoose.set('useUnifiedTopology', true); 
+mongoose.connect("mongodb://localhost/auth_demo_app"); 
+  
+var app = express(); 
+app.set("view engine", "ejs"); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+  
+app.use(require("express-session")({ 
+    secret: "Rusty is a dog", 
+    resave: false, 
+    saveUninitialized: false
+})); 
+  
+app.use(passport.initialize()); 
+app.use(passport.session()); 
+  
+passport.use(new LocalStrategy(User.authenticate())); 
+passport.serializeUser(User.serializeUser()); 
+passport.deserializeUser(User.deserializeUser()); 
 
+app.get("/", function (req, res) { 
+    res.render("home"); 
+}); 
 
-//add other middleware
-app.use(cors());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-
-app.get('/', (req, res) => {
-    res.sendFile(filesDir + 'login.html')
-})
-app.get('/register', (req, res) => {
-    res.sendFile(filesDir + 'register.html')
-})
-app.listen(port, () => console.log(`Server running okay!`))
-
-app.post('/register', function (req, res) {
-    console.log("NEW POST!!\n" + req.body.name, req.body.mail, req.body.pass)
-    dbfile.data(req.body.name, req.body.mail, req.body.pass)
-    res.send("Created person " + req.body.name)
-    res.end()
-})
+  app.get("/secret", isLoggedIn, function (req, res) { 
+    res.render("secret"); 
+}); 
+  
+app.get("/register", function (req, res) { 
+    res.render("register"); 
+}); 
+  
+app.post("/register", function (req, res) { 
+    var username = req.body.username 
+    var password = req.body.password 
+    User.register(new User({ username: username }), 
+            password, function (err, user) { 
+        if (err) { 
+            console.log(err); 
+            return res.render("register"); 
+        } 
+  
+        passport.authenticate("local")( 
+            req, res, function () { 
+            res.render("secret"); 
+        }); 
+    }); 
+}); 
+  
+app.get("/login", function (req, res) { 
+    res.render("login"); 
+}); 
+  
+app.post("/login", passport.authenticate("local", { 
+    successRedirect: "/secret", 
+    failureRedirect: "/login"
+}), function (req, res) { 
+}); 
+  
+app.get("/logout", function (req, res) { 
+    req.logout(); 
+    res.redirect("/"); 
+}); 
+  
+function isLoggedIn(req, res, next) { 
+    if (req.isAuthenticated()) return next(); 
+    res.redirect("/login"); 
+} 
+  
+var port = process.env.PORT || 3000; 
+app.listen(port, function () { 
+    console.log("Server Has Started!"); 
+}); 
---
+++ LoginTesting/models/user.js
@@ -0,0 +1,23 @@
+var mongoose 	=	 require('mongoose');
+
+mongoose.connect('mongodb://localhost/nodeauth');
+
+var db 			=	 mongoose.connection;
+
+
+var UserSchema 	=  mongoose.Schema({
+	username : {
+		type 	: String,
+		index	: true
+	},
+	password :	{
+		type 	: String, 
+		required: true, 
+		bcrypt  : true
+	},
+	email 		: String,
+	name 		: String,
+	profile 	: String
+});
+
+module.exports =  mongoose.model('User', UserSchema);
