25761b83a3418ffa3148e468959991228691603c
linusromland <48645689+linusromland@users.noreply.github.com>
2020-10-19 12:04:15 +0200

    new port

--- Vecka42/app.js
+++ Vecka42/app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const dBModule = require('./dBModule')
 const app = express()
-const port = 3000
+const port = 3001
 
 const clientDir = __dirname + "/client/"
 
